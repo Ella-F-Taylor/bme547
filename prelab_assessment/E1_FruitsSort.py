06b9357d09b45a789ee5191d9d0db6239105c177
Ella Taylor <taylor.ellafay@gmail.com>
2026-01-13 00:35:34 -0500

    added a starting comment to E1!

--- prelab_assessment/E1_FruitsSort.py
+++ prelab_assessment/E1_FruitsSort.py
@@ -1,14 +1,16 @@
+#Hello! Throughout these pre assignments, I did use use the Python resources file & stack exchange to learn and help me, so any comments or advice are appreciated if you have time! Thank you!
+
 #exercise 1: sort the fruits and veggies by string size
 import pandas as pd
 from pathlib import Path
 
 #file path to the fruits txt file
-HERE = Path(__file__).parent #tells me to go to the directory where this file is located (and look at everything in the parent folder)
+HERE = Path(__file__).parent #tells me to go to the directory where this file is located (and look at everything in the parent folder, had to look this up in resources)
 #fruits_csv = ('C:/Users/taylo/github/bme547/prelab_assessment/fruits.txt') #old absolute path
 fruits_csv = HERE / 'fruits.txt' #find fruits.txt in that parent folder
 
 fruits_df = pd.read_csv(fruits_csv, header=None)
-fruits_list = fruits_df.iloc[0].str.strip().tolist()
+fruits_list = fruits_df.iloc[0].str.strip().tolist() #iloc[0] for the 1st index
 print("whole list", fruits_list)
 
 #max character length of the strings
