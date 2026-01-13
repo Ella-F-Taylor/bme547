72f00639d317c8bc446367642fba60ca4dfed85d
Ella Taylor <taylor.ellafay@gmail.com>
2026-01-13 12:28:17 -0500

    Moved Python Test to prelab folder

---
+++ prelab_assessment/Python_Test.py
@@ -0,0 +1,12 @@
+#first python file test
+x = [1, 2, 3, 4, "banana"]
+
+#to access all of the variables like Spyder could, click the outline tab on the lower left
+#then run the code using run current file in interactive window
+#then look at the jupyter tab on the bottom
+
+import numpy as np
+y = np.array([5, 6, 7, 8, 9])
+print(np.__version__)
+#test test test
+
